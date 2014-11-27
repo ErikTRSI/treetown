02d4e10f065e5096154af075f9b6bc512ebda9ec
Erik Krietsch <erik@totallyradicalsoftware.com>
2014-11-26 22:41:18 -0600

    WIP. Add layout, navbar, early routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get 'pages/:slug' => 'pages#show'
+  get 'pages/:slug' => 'pages#show', as: :page
   root 'pages#show', { slug: "home" }
 
   # Example of regular route:
