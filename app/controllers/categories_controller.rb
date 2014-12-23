8f70b77c2485b30241660aa4ee5cc87af117f441
Erik Krietsch <erik.krietsch@gmail.com>
2014-12-23 17:31:14 -0600

    Add controller methods for Category management.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,27 @@
 class CategoriesController < ApplicationController
-  before_action :verify_user
+  before_action :auth_check
+  before_action :find_category_or_redirect, except: [:index]
   def index
     @categories = Category.all
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+    @category.destroy
+  end
+
   private
-  def verify_user
-    current_user
+
+  def find_category_or_redirect
+    @category = Category.find(params[:id])
+    redirect_to :index unless @category
   end
 end
